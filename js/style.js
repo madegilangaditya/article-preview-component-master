52ce6c4e78ade249bd84ad1363e266fea1e6b215
gilang aditya <emosidan@gilangs-MacBook-Air.local>
2021-05-28 15:36:55 +0800

    first commit

---
+++ js/style.js
@@ -0,0 +1,5 @@
+document.querySelector(".share-btn").addEventListener('click', function () {
+    document.querySelector(".share-popup").classList.toggle('active');
+    document.querySelector(".share-popup.mobile").classList.toggle('active');
+});
+    
